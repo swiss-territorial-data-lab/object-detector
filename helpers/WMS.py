6a35af1c4e10fb79cd86df456919f6a0819d7660
waldstrom <adrian.meyer@fhnw.ch>
2020-11-13 11:11:26 +0100

    wms exchanged
    Former-commit-id: f4bbcbd02d029a96715d80843b3ab6fd5fad1168

--- helpers/WMS.py
+++ helpers/WMS.py
@@ -80,48 +80,75 @@ def image_metadata_to_affine_transform(image_metadata):
 
 
 def get_geotiff(WMS_url, bbox, width, height, filename, imageSR=2056, bboxSR=2056, save_metadata=False, overwrite=True):
-    """
-        by default, bbox must be in EPSG:2056
-    """
+    ROOT_URL = WMS_url
+	BBOX = bbox
+	WIDTH=width
+	HEIGHT=height
+	LAYERS = "ortho2019"
+	SRS="EPSG:900913"
+	OUTPUT_DIR = 'output-NE'
+	# let's make the output directory in case it doesn't exist
+	if not os.path.exists(OUTPUT_DIR):
+		os.makedirs(OUTPUT_DIR)
 
-    if not filename.endswith('.tif'):
-        raise Exception("Filename must end with .tif")
+	PNG_FILENAME = os.path.join(OUTPUT_DIR, filename)
+
+	# image download
+
+	params = dict(
+		service="WMS",
+		version="1.1.1",
+		request="GetMap",
+		layers=LAYERS,
+		format="image/png",
+		srs=SRS,
+		transparent=True,
+		styles="",
+		bbox=BBOX,
+		width=WIDTH,
+		height=HEIGHT,
+		#exceptions="application/vnd.ogc.se_inimage",
+	)
+
+	r = requests.get(ROOT_URL, params=params)
+	with open(f'{PNG_FILENAME}', 'wb') as fp:
+		fp.write(r.content)
+
+	image_metadata = r.json()
+
+	# PGW file generation
+
+	xmin, ymin, xmax, ymax = [float(x) for x in BBOX.split(',')]
+
+	affine = from_bounds(xmin, ymin, xmax, ymax, WIDTH, HEIGHT)
+
+	a = affine.a
+	b = affine.b
+	c = affine.c
+	d = affine.d
+	e = affine.e
+	f = affine.f
+
+	c += a/2.0 # <- IMPORTANT
+	f += e/2.0 # <- IMPORTANT
+
+	pgw = "\n".join([str(a), str(d), str(b), str(e), str(c), str(f)+"\n"])
+	print(pgw)
+
+	with open(PNG_FILENAME.replace('.png', '.pgw'), 'w') as fp:
+		fp.write(pgw)
+
+	# PNG + PGW => GeoTIFF
+
+	src_ds = gdal.Open(PNG_FILENAME)
+	gdal.Translate(PNG_FILENAME.replace('.png', '.tif'), src_ds, options=f'-of GTiff -a_srs {SRS}')
+	src_ds = None 
+
+	print('Output GeoTIFF:', PNG_FILENAME.replace('.png', '.tif'))
 
     tiff_filename = filename.replace('.tif', '_.tif')
-    tfw_filename  = filename.replace('.tif', '_.tfw')
     md_filename   = filename.replace('.tif', '.json')
     geotiff_filename = f"{filename}"
-
-    if not overwrite and os.path.isfile(geotiff_filename):
-        return None
-
-
-    params = {'bbox': bbox, 'format': 'tif', 'size': f'{width},{height}', 'f': 'pjson', 'imageSR': imageSR, 'bboxSR': bboxSR}
-    res = requests.post(WMS_url + '/export', data=params, verify=False)
-    image_metadata = res.json()
-
-    #print('Computing world file...')
-    tfw = image_metadata_to_tfw(image_metadata)
-
-    with open(tfw_filename, 'w') as fp:
-        fp.write(tfw)
-
-    #print('Fetching image...')
-    url = image_metadata['href']
-    r = requests.get(url, allow_redirects=True, verify=False)
-    #print(f'Writing image file: {tiff_filename}')
-    with open(tiff_filename, 'wb') as fp:
-        fp.write(r.content)
-
-    #print(f'TIFF -> GeoTIFF: {geotiff_filename}')
-    src_ds = gdal.Open(tiff_filename)
-    #dst_ds = gdal.Translate(geotiff_filename, src_ds, options='-of GTiff -a_srs EPSG:2056 -a_nodata 253')
-    gdal.Translate(geotiff_filename, src_ds, options=f'-of GTiff -a_srs EPSG:{imageSR}')
-    #dst_ds = None
-    src_ds = None 
-
-    os.remove(tiff_filename)
-    os.remove(tfw_filename)
 
     if save_metadata:
         with open(md_filename, 'w') as fp:
