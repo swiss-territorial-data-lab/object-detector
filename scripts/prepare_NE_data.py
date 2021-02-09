8d6853b4e3ca053de69c898b3843b1668a0652c8
Alessandro Cerioni <alessandro.cerioni@etat.ge.ch>
2021-02-09 10:40:16 +0100

    Adding data preparation scripts, missing in the previous commit

---
+++ scripts/prepare_NE_data.py
@@ -0,0 +1,131 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+import logging.config
+import time
+import argparse
+import yaml
+import os, sys, inspect
+import requests
+import geopandas as gpd
+import pandas as pd
+import json
+
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+# the following allows us to import modules from within this file's parent folder
+sys.path.insert(0, '.')
+from helpers import WMS
+from helpers import COCO
+from helpers import misc
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('root')
+
+if __name__ == "__main__":
+
+
+    tic = time.time()
+    logger.info('Starting...')
+
+    parser = argparse.ArgumentParser(description="This script prepares datasets for the Neuchâtel's Swimming Pools detection task.")
+    parser.add_argument('config_file', type=str, help='a YAML config file')
+    args = parser.parse_args()
+
+    logger.info(f"Using {args.config_file} as config file.")
+
+    with open(args.config_file) as fp:
+        cfg = yaml.load(fp, Loader=yaml.FullLoader)[os.path.basename(__file__)]
+
+    # TODO: check whether the configuration file contains the required information
+    OUTPUT_DIR = cfg['output_folder']
+    # sectors
+    GROUND_TRUTH_SECTORS_SHPFILE = cfg['datasets']['ground_truth_sectors_shapefile']
+    OTHER_SECTORS_SHPFILE = cfg['datasets']['other_sectors_shapefile']
+    # swimming pools
+    GROUND_TRUTH_SWIMMING_POOLS_SHPFILE = cfg['datasets']['ground_truth_swimming_pools_shapefile']
+    OTHER_SWIMMING_POOLS_SHPFILE = cfg['datasets']['other_swimming_pools_shapefile']
+    ZOOM_LEVEL = cfg['zoom_level']
+    
+    # let's make the output directory in case it doesn't exist
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    written_files = []
+
+
+    # ------ Loading datasets
+
+    dataset_dict = {}
+
+    for dataset in [
+        'ground_truth_sectors', 
+        'other_sectors',
+        'ground_truth_swimming_pools',
+        'other_swimming_pools']:
+
+        shpfile = eval(f'{dataset.upper()}_SHPFILE')#.split('/')[-1]
+
+        # TODO: check file integrity (ex.: md5sum)
+        logger.info(f"Loading the {dataset} dataset as a GeoPandas DataFrame...")
+        dataset_dict[dataset] = gpd.read_file(f'{shpfile}')
+        logger.info(f"...done. {len(dataset_dict[dataset])} records were found.")
+
+
+    # ------ Computing the Area of Interest (AOI)
+
+    aoi_gdf = pd.concat([
+        dataset_dict['ground_truth_sectors'],
+        dataset_dict['other_sectors']
+    ])
+
+    aoi_gdf.drop_duplicates(inplace=True)
+
+    AOI_GEOJSON = os.path.join(OUTPUT_DIR, "aoi.geojson")
+    try:
+        aoi_gdf.to_crs(epsg=4326).to_file(AOI_GEOJSON, driver='GeoJSON', encoding='utf-8')
+        written_files.append(AOI_GEOJSON)
+    except Exception as e:
+        logger.error(f"Could not write to file {AOI_GEOJSON}. Exception: {e}")    
+
+    AOI_TILES_GEOJSON = os.path.join(OUTPUT_DIR, f"aoi_z{ZOOM_LEVEL}_tiles.geojson")
+    
+    if not os.path.isfile(AOI_TILES_GEOJSON):
+        logger.info(f"You should now open a Linux shell and run the following command from the working directory (./{OUTPUT_DIR}), then run this script again:")
+        logger.info(f"cat aoi.geojson | supermercado burn {ZOOM_LEVEL} | mercantile shapes | fio collect > aoi_z{ZOOM_LEVEL}_tiles.geojson")
+        sys.exit(0) 
+        
+    else:
+        logger.info("Loading AoI tiles as a GeoPandas DataFrame...")
+        aoi_tiles_gdf = gpd.read_file(AOI_TILES_GEOJSON)
+        logger.info(f"...done. {len(aoi_tiles_gdf)} records were found.")
+
+
+    assert ( len(aoi_tiles_gdf.drop_duplicates(subset='id')) == len(aoi_tiles_gdf) ) # make sure there are no duplicates
+
+
+    # ------ Exporting labels to GeoJSON
+
+    GT_LABELS_GEOJSON = os.path.join(OUTPUT_DIR, f'ground_truth_labels.geojson')
+    OTH_LABELS_GEOJSON = os.path.join(OUTPUT_DIR, f'other_labels.geojson')
+
+    dataset_dict['ground_truth_swimming_pools'].to_crs(epsg=4326).to_file(GT_LABELS_GEOJSON, driver='GeoJSON')
+    written_files.append(GT_LABELS_GEOJSON)
+    dataset_dict['other_swimming_pools'].to_crs(epsg=4326).to_file(OTH_LABELS_GEOJSON, driver='GeoJSON')
+    written_files.append(OTH_LABELS_GEOJSON)
+
+    print()
+    logger.info("The following files were written. Let's check them out!")
+    for written_file in written_files:
+        logger.info(written_file)
+    print()
+
+    toc = time.time()
+    logger.info(f"Nothing left to be done: exiting. Elapsed time: {(toc-tic):.2f} seconds")
+
+    sys.stderr.flush()
+
+
+
