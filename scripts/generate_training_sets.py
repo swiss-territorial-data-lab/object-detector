26b21461c034744f5ec62ba6cd1223749ed7ffac
Alessandro Cerioni <alessandro.cerioni@etat.ge.ch>
2021-02-24 13:18:12 +0100

    Completed the switch to the 'multiprocessing' joblib backend

--- scripts/generate_training_sets.py
+++ scripts/generate_training_sets.py
@@ -246,7 +246,7 @@ if __name__ == "__main__":
     logger.info("...done.")
 
     logger.info(f"Executing tasks, {N_JOBS} at a time...")
-    job_outcome = Parallel(n_jobs=N_JOBS)(
+    job_outcome = Parallel(n_jobs=N_JOBS, backend="multiprocessing")(
             delayed(image_getter)(**v) for k, v in tqdm( sorted(list(job_dict.items())) )
     )
     logger.info("Checking whether all the expected tiles were actually downloaded...")
