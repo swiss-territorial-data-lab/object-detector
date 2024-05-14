8a6796ae581e049cc1347ba50636b5283a8c4760
Clemence Herny <clemence.herny@gmail.com>
2024-05-14 15:16:42 +0000

    Select empty tiles geom for an AoI

--- examples/quarry-detection/prepare_data.py
+++ examples/quarry-detection/prepare_data.py
@@ -16,6 +16,7 @@ sys.path.insert(0, '../..')
 from helpers.misc import format_logger
 from helpers.constants import DONE_MSG
 
+from tqdm import tqdm
 from loguru import logger
 logger = format_logger(logger)
 
@@ -30,13 +31,11 @@ def add_tile_id(row):
 
 if __name__ == "__main__":
 
-    # Start chronometer
     tic = time.time()
     logger.info('Starting...')
 
-    # Argument and parameter specification
     parser = argparse.ArgumentParser(description="The script prepares the Mineral Extraction Sites dataset to be processed by the object-detector scripts")
-    parser.add_argument('config_file', type=str, help='Framework configuration file')
+    parser.add_argument('config_file', type=str, help="Framework configuration file")
     args = parser.parse_args()
 
     logger.info(f"Using {args.config_file} as config file.")
@@ -49,6 +48,14 @@ if __name__ == "__main__":
     SHPFILE = cfg['datasets']['shapefile']
     ZOOM_LEVEL = cfg['zoom_level']
 
+    if 'empty_tiles' in cfg.keys():        
+        EMPTY_TILES = cfg['empty_tiles']['enable']
+        if EMPTY_TILES:
+            NB_TILES_FRAC = cfg['empty_tiles']['tiles_frac']
+            AOI = cfg['empty_tiles']['aoi']
+    else:
+        EMPTY_TILES = None
+
     # Create an output directory in case it doesn't exist
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
@@ -57,15 +64,15 @@ if __name__ == "__main__":
     
     # Prepare the tiles
 
-    ## Convert datasets shapefiles into geojson format
-    logger.info('Convert labels shapefile into GeoJSON format (EPSG:4326)...')
+    # Convert datasets shapefiles into geojson format
+    logger.info("Convert labels shapefile into GeoJSON format (EPSG:4326)...")
     labels = gpd.read_file(SHPFILE)
     labels_4326 = labels.to_crs(epsg=4326)
     labels_4326['CATEGORY'] = "quarry"
     labels_4326['SUPERCATEGORY'] = "land usage"
 
     nb_labels = len(labels)
-    logger.info('There is/are ' + str(nb_labels) + ' polygon(s) in ' + SHPFILE)
+    logger.info(f"There are {nb_labels} polygons in {SHPFILE}")
 
     label_filename = 'labels.geojson'
     label_filepath = os.path.join(OUTPUT_DIR, label_filename)
@@ -73,18 +80,17 @@ if __name__ == "__main__":
     written_files.append(label_filepath)  
     logger.success(f"{DONE_MSG} A file was written: {label_filepath}")
 
-    logger.info('Creating tiles for the Area of Interest (AoI)...')   
+    logger.info("Creating tiles for the Area of Interest (AoI)...")   
     
     # Grid definition
     tms = morecantile.tms.get("WebMercatorQuad")    # epsg:3857
 
-    # New gpd with only labels geometric info (minx, miny, maxx, maxy) 
-    logger.info('- Get geometric boundaries of the label(s)')  
+    # Keep only label boundary geometry info (minx, miny, maxx, maxy) 
+    logger.info("- Get the label geometric boundaries")  
     label_boundaries_df = labels_4326.bounds
 
-    # Iterate on geometric coordinates to defined tiles for a given label at a given zoom level
-    # A gpd is created for each label and are then concatenate into a single gpd 
-    logger.info('- Compute tiles for each label(s) geometry') 
+    # Find coordinates of tiles intersecting labels
+    logger.info("- Compute tiles intersecting labels") 
     tiles_4326_all = [] 
 
     for label_boundary in label_boundaries_df.itertuples():
@@ -94,30 +100,53 @@ if __name__ == "__main__":
         tiles_4326_all.append(tiles_4326)
     tiles_4326_aoi = gpd.GeoDataFrame(pd.concat(tiles_4326_all, ignore_index=True))
 
-    # Remove unrelevant tiles and reorganised the data set:
-    logger.info('- Remove duplicated tiles and tiles that are not intersecting labels') 
+    logger.info("- Remove duplicated tiles and tiles that are not intersecting labels") 
 
-    # - Keep only tiles that are actually intersecting labels
+    # Keep tiles that are intersecting labels
     labels_4326.rename(columns={'FID': 'id_aoi'}, inplace=True)
     tiles_4326 = gpd.sjoin(tiles_4326_aoi, labels_4326, how='inner')
 
-    # - Remove duplicated tiles
+    # Remove duplicated tiles
     if nb_labels > 1:
         tiles_4326.drop_duplicates('title', inplace=True)
+    nb_tiles = len(tiles_4326)
+    logger.info(f"- Number of tiles = {nb_tiles}")
 
-    # - Remove useless columns, reset feature id and redefine it according to xyz format  
-    logger.info('- Add tile IDs and reorganise data set')
+    # Add tiles not intersecting with labels to the dataset 
+    if EMPTY_TILES:
+
+        nb_empty_tiles = int(NB_TILES_FRAC * nb_tiles)
+        logger.info(f"- Add {int(NB_TILES_FRAC * 100)}% of empty tiles = {nb_empty_tiles} empty tiles")
+
+        aoi = gpd.read_file(AOI)
+        aoi_4326 = aoi.to_crs(epsg=4326)
+        
+        logger.info("- Get AoI geometric boundaries")  
+        aoi_boundaries_df = aoi_4326.bounds
+ 
+        logger.info("- Select random empty tiles") 
+        aoi_tiles_4326_all = [] 
+        for aoi_boundary in aoi_boundaries_df.itertuples():
+            coords = (aoi_boundary.minx, aoi_boundary.miny, aoi_boundary.maxx, aoi_boundary.maxy)      
+            aoi_tiles_4326 = gpd.GeoDataFrame.from_features([tms.feature(x, projected=False) for x in tms.tiles(*coords, zooms=[ZOOM_LEVEL])]) 
+            aoi_tiles_4326.set_crs(epsg=4326, inplace=True)
+            aoi_tiles_4326_all.append(aoi_tiles_4326)
+        empty_tiles_4326_aoi = gpd.GeoDataFrame(pd.concat(aoi_tiles_4326_all, ignore_index=True))
+
+        # Filter tiles intersecting labels 
+        empty_tiles_4326_aoi = empty_tiles_4326_aoi[~empty_tiles_4326_aoi['title'].isin(tiles_4326['title'])].sample(n=nb_empty_tiles, random_state=1)
+
+        tiles_4326 = pd.concat([tiles_4326, empty_tiles_4326_aoi])
+
+    # Add tile IDs and reorganise data set
     tiles_4326 = tiles_4326[['geometry', 'title']].copy()
     tiles_4326.reset_index(drop=True, inplace=True)
-
-    # Add the ID column
     tiles_4326 = tiles_4326.apply(add_tile_id, axis=1)
     
     nb_tiles = len(tiles_4326)
-    logger.info('There was/were ' + str(nb_tiles) + ' tiles(s) created')
+    logger.info(f"There were {nb_tiles} tiles created")
 
-    # Export tiles to GeoJSON
-    logger.info('Export tiles to GeoJSON (EPSG:4326)...')  
+    logger.info("Export tiles to GeoJSON (EPSG:4326)...")  
     tile_filename = 'tiles.geojson'
     tile_filepath = os.path.join(OUTPUT_DIR, tile_filename)
     tiles_4326.to_file(tile_filepath, driver='GeoJSON')
@@ -130,7 +159,6 @@ if __name__ == "__main__":
         logger.info(written_file)
     print()
 
-    # Stop chronometer  
     toc = time.time()
     logger.info(f"Nothing left to be done: exiting. Elapsed time: {(toc-tic):.2f} seconds")
     
