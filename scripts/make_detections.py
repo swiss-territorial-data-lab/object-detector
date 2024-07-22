b01d34be2d9a1454cf9ab3142cdd545d22a6c3af
Clemence Herny <clemence.herny@gmail.com>
2024-07-22 15:27:24 +0000

    Correct error in year

--- scripts/make_detections.py
+++ scripts/make_detections.py
@@ -138,18 +138,17 @@ def main(cfg_file_path):
             # let's make sure all the images share the same CRS
             if crs is not None: # iterations other than the 1st
                 assert crs == _crs, "Mismatching CRS"
-
             crs = _crs
 
             transform = image_metadata_to_affine_transform(im_md)
-            if 'year_img' in im_md.keys():
-                year = im_md['year_img']
+            if 'img_year' in im_md.keys():
+                year = im_md['img_year']
                 this_image_feats = detectron2dets_to_features(outputs, crs, transform, RDP_SIMPLIFICATION_ENABLED, RDP_SIMPLIFICATION_EPSILON, year=year)
             else:
                 this_image_feats = detectron2dets_to_features(outputs, crs, transform, RDP_SIMPLIFICATION_ENABLED, RDP_SIMPLIFICATION_EPSILON)
 
             all_feats += this_image_feats
-
+  
         gdf = gpd.GeoDataFrame.from_features(all_feats)
         gdf['dataset'] = dataset
         gdf.crs = crs
