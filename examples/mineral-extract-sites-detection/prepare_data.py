71d0bfb25aa80a1a9992a101e98d17691a6dd6ad
Clemence Herny <clemence.herny@gmail.com>
2024-10-28 13:22:38 +0000

    Correct error wrong expression in prep_data

--- examples/mineral-extract-sites-detection/prepare_data.py
+++ examples/mineral-extract-sites-detection/prepare_data.py
@@ -262,7 +262,7 @@ if __name__ == "__main__":
         if FP_SHPFILE:
             tiles_4326_aoi_gdf = pd.concat([tiles_4326_gt_gdf, tiles_fp_4326_gdf]) 
         else:
-            tiles_4326_gt_gdf.copy()
+            tiles_4326_aoi_gdf = tiles_4326_gt_gdf.copy()
 
     # Get all the tiles in one gdf 
     if EPT_SHPFILE and aoi_bbox.contains(labels_bbox) == False:
